7c8b33af0b7794c34dfbb583af8e1889d5b145de
John Wood <john@johnpwood.net>
2013-04-12 14:19:42 -0500

    Add option parsing

--- lib/capssh.rb
+++ lib/capssh.rb
@@ -3,26 +3,25 @@ require 'capistrano'
 
 class Capssh
 
-  def self.execute(environment, role=nil)
-    if environment.nil? || environment.empty?
-      display_error_and_exit("environment must be specified", true)
-    end
-
-    if environment =~ /help/
-      display_usage
-      exit
-    end
-
-    config_file = "./config/deploy/#{environment}.rb"
-    display_error_and_exit("No stage file exists for '#{environment}'") unless File.exist?(config_file)
+  def self.execute(options={})
+    environment = options[:environment]
+    role = options[:role]
 
     config = Capistrano::Configuration.new
     config.load(:file => "./config/deploy.rb")
-    config.load(:file => config_file)
 
-    role = role.nil? ? :app : role.to_sym
+    if environment
+      config_file = "./config/deploy/#{environment}.rb"
+      display_error_and_exit("No stage file exists for '#{environment}'") unless File.exist?(config_file)
+      config.load(:file => config_file)
+    end
+
+    if config.find_servers.empty?
+      display_error_and_exit("Please speicfy a valid environment: #{valid_environments.join(", ")}")
+    end
+
     servers = config.find_servers(:roles => role)
-    display_error_and_exit("no servers could be found for environment '#{environment}' and role ':#{role}'") if servers.empty?
+    display_error_and_exit("no servers could be found for environment '#{environment}' and role '#{role}'") if servers.empty?
 
     user = config[:user] || ENV['USER']
     server = servers.first
@@ -30,14 +29,13 @@ class Capssh
     exec "ssh #{user}@#{server}"
   end
 
-  def self.display_error_and_exit(error, should_display_usage=false)
+  def self.display_error_and_exit(error)
     puts "ERROR: #{error}"
-    display_usage if should_display_usage
     exit(1)
   end
 
-  def self.display_usage
-    puts "Usage: capssh <environment> [role]"
+  def self.valid_environments
+    Dir.entries("./config/deploy").select { |f| f =~ /.rb$/ }.map { |f| f.sub(".rb", "") }
   end
 
 end
